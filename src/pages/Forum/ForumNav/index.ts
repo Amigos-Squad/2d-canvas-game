6f80499aa323810d1cba6a7237e1531f17bef72b
Victoria Katysheva <katysheva.victoria@gmail.com>
2021-09-23 00:29:01 +0300

    first draft forum page

---
+++ src/pages/Forum/ForumNav/index.ts
@@ -0,0 +1,1 @@
+export { ForumNav } from './ForumNav';
---
+++ src/pages/Forum/Topics/TopicItem.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+import { TopicOpenIcon } from '@/components/Icons/SVG/TopicOpen';
+
+import './Topics.scss';
+
+export const TopicItem = (): ReactElement => (
+    <div className="topic-item">
+        <div className="topic-item__statusIcon"><TopicOpenIcon/></div>
+        <div className="topic-item__title">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</div>
+        <div className="topic-item__posts">2031</div>
+        <div className="topic-item__starter">Kuart</div>
+        <div className="topic-item__lastMessage">16:42</div>
+    </div>
+);
