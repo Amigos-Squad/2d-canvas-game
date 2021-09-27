414c8093ec6446d4eb49de9b537211ba572911ec
Victoria Katysheva <katysheva.victoria@gmail.com>
2021-09-27 22:41:55 +0300

    create profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,10 +1,26 @@
 import React, { ReactElement, memo } from 'react';
 import { Page } from '@/modules';
+import { AvatarIcon } from '@/components/Icons/SVG/Avatar';
+import { ProfileNav } from '@/modules/Profile/ProfileNav';
+
+import'./Profile.scss';
 
 export const Profile = memo(
   (): ReactElement => (
     <Page withHeader>
-      <div>Profile</div>
+      <div className="profile-page">
+          <ProfileNav/>
+          <div className="avatar">
+              <AvatarIcon/>
+          </div>
+          <div className="user">
+              USER NAME
+          </div>
+          <div className="score">
+              <div>max score</div>
+              <div>4004</div>
+          </div>
+      </div>
     </Page>
   )
 );
