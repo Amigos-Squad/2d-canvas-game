737b8eac6f6bbefd49af3a7c7e4c477557796f6e
Alexandr Kvasnikov <videot4pe@yandex.ru>
2021-10-14 18:25:26 +0300

    fix animation

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,32 +1,36 @@
-import React, { memo, ReactElement } from 'react';
+import React, { memo, useRef, ReactElement } from 'react';
 import { Switch, useLocation } from 'react-router-dom';
 import { Route } from 'react-router';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { ErrorBoundaryWithRouter } from '@/components';
 import { AuthProvider } from './AuthProvider';
 import { USED_ROUTES } from './routes';
 import './App.scss';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export const App = memo(
   (): ReactElement => {
     const location = useLocation();
-    
+    const nodeRef = useRef(null)
+
     return (
       <AuthProvider>
         <ErrorBoundaryWithRouter>
-          <TransitionGroup>
+          <TransitionGroup nodeRef={nodeRef}>
             <CSSTransition
+              nodeRef={nodeRef}
               key={location.key}
               classNames="fade"
-              timeout={500}
+              timeout={300}
             >
-              <Switch location={location}>
-                {USED_ROUTES.map(({ path, exact, component }) => (
-                  <Route path={path} key={path} exact={exact}>
-                    {component}
-                  </Route>
-                ))}
-              </Switch>
+              <div className="switch-container" ref={nodeRef}>
+                <Switch location={location} ref={nodeRef}>
+                  {USED_ROUTES.map(({ path, exact, component }) => (
+                    <Route path={path} key={path} exact={exact}>
+                      {component}
+                    </Route>
+                  ))}
+                </Switch>
+              </div>
             </CSSTransition>
           </TransitionGroup>
         </ErrorBoundaryWithRouter>
