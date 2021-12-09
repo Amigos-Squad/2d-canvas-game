6da4ee1f0ae80835ee2baa194ee4951283c39fd8
Alexandr Kvasnikov <videot4pe@yandex.ru>
2021-12-09 10:49:15 +0300

    url

--- src/api/http/const.ts
+++ src/api/http/const.ts
@@ -1,6 +1,5 @@
-import { ENVS } from 'config/webpack/assets/env';
 
 export const YANDEX_API = 'https://ya-praktikum.tech/api/v2';
 export const OWN_API = 'https://acapulco-spacestory-08.ya-praktikum.tech/api/v1';
 export const BASE_API = '/api/v1';
-export const HOME_URL = ENVS.__DEV__ ? 'http://localhost:3000/api/v1' : 'https://acapulco-spacestory-08.ya-praktikum.tech/api/v1';
+export const HOME_URL = 'https://acapulco-spacestory-08.ya-praktikum.tech/api/v1';
